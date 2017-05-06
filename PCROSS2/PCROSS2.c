3a65d0fe6191a5fd51c3ae242a0823ad4eb401a9
Alif Hilmi Akbar <alifhilmirc@gmail.com>
2017-05-06 07:45:53 +0700

    Add files via upload

---
+++ PCROSS2/PCROSS2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int n, i, j, r, c, x, y;
+	scanf("%d", &n);
+	while(n--) {
+		scanf("%d %d %d %d", &r, &c, &x, &y);
+		for(i = 1; i <= r; i++) {
+			for(j = 1; j <= c; j++) {
+				if(i + j == x + y || i - j == x - y) {
+					printf("*");
+				}
+				else {
+					printf(".");
+				}
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
